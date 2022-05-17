1146c13c5928ca8ff15c9e3dd8ecaf5ef972801d
Jachane James <49769620+iJKJames@users.noreply.github.com>
2022-05-16 20:03:25 -0400

    Need to review loops for exercise 02. Assignment not complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
 const repeatString = function() {
-
+    string = 'hey';
+    for (num = 0; num < 3; num++) {
+        theString = '';
+        theString += string;
+        return theString;
+    }
 };
 
 // Do not edit below this line
