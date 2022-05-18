b45479b4f9c23db3e6e047e90f6e58ffb4d74bb6
Jachane James <49769620+iJKJames@users.noreply.github.com>
2022-05-17 20:24:56 -0400

    Completed exercise 2. 7 of 7 tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,13 @@
-const repeatString = function() {
-    string = 'hey';
-    for (num = 0; num < 3; num++) {
-        theString = '';
-        theString += string;
-        return theString;
+const repeatString = function(string, num) {
+    theString = '';
+    if (num < 0) {
+        theString = 'ERROR';
+    } else {
+    for (i = 0; i < num; i++) {
+        theString = theString += string;
     }
+    }
+    return theString
 };
 
 // Do not edit below this line
