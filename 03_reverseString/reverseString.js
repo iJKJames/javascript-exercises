152ced351c3f47f8a3bf5529be6354ba7c497932
Jachane James <49769620+iJKJames@users.noreply.github.com>
2022-05-18 20:30:34 -0400

    4 of 4 tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
 const reverseString = function(string) {
 toBackwards = '';
 toBackwards = string.split('');
+
+toBackwards.reverse();
+
+return toBackwards.join('');
 };
 
 // Do not edit below this line
