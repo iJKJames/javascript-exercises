f6c433db2bf63ffc1d00bb43abe64170ed30a7e2
Jachane James <49769620+iJKJames@users.noreply.github.com>
2022-05-19 22:14:57 -0400

    Still 0 of 7. Seemed to remove args, but seeing additional array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,11 @@
 const removeFromArray = function(array, ...args) {
-    array = [];
-    array = array.splice(args);
-    return array;    
+    for (i = 0; i < array.length; i++) {
+        if (args > -1) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
