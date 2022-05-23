e4171305dec4eed3d0a071bf75395d3ca00eb256
Jachane James <49769620+iJKJames@users.noreply.github.com>
2022-05-23 12:05:00 -0400

    2 of 6 passed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function(...args) {
-    
+const sumAll = function(int1, int2) {
+    numbers = [];
+    sum = 0;
+    for (i = int1; i <= int2; i++) {
+        numbers.push(i);
+    }
+    for (i of numbers) {
+        sum += i;
+    }
+    return sum;
+
+
+    //initialize int1
+
+    //initialize midInts
+
+    //loop through midInts
+    //add i to sum
+
+    //add int2
+
+    //return sum
 };
 
 // Do not edit below this line
