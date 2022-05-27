591f24b671159e451e42f15a95d4da16a2898734
Jachane James <49769620+iJKJames@users.noreply.github.com>
2022-05-26 21:02:44 -0400

    7 of 7 passed. Converted if statement to switch statement

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,28 +3,29 @@ const sumAll = function (a, b) {
     sum = 0;
     bigNum = '';
     i = '';
-
     (a > b) ? bigNum = a : bigNum = b;
     (a > b) ? i = b : i = a;
-
-
-    for (; i <= bigNum; i++) {
-        if (a < 0 || b < 0) {
-            sum = 'ERROR';
-            break;
-        } else if (typeof(a) != 'number' || typeof (b) != 'number') {
-            sum = 'ERROR';
-            break;
-        } else {
-            numbers.push(i);
-        }
-    }
-    for (i of numbers) {
-        sum += i;
-    }
     
-    return sum;
-};
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        sum = 'ERROR';
+        return sum;
+    } else {
 
-// Do not edit below this line
-module.exports = sumAll;
+        for (; i <= bigNum; i++) {        
+            switch (true) {
+                    case a < 0 || b < 0:
+                    case typeof (a) != 'number' || typeof (b) != 'number':
+                        sum = 'ERROR'
+                        break;
+                    default:
+                        numbers.push(i)
+                }
+            }
+        for (i of numbers) {
+            sum += i;
+        }
+        return sum;
+    };
+};
+    // Do not edit below this line
+    module.exports = sumAll;
