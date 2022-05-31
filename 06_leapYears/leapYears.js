2894b7ed8b50c7f51b0fa32f88bedcc5593691cd
Jachane James <49769620+iJKJames@users.noreply.github.com>
2022-05-30 21:53:03 -0400

    1 of 6 passed.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    year = '';
 
+    if (year % 4 == 0 && year % 400 == 0) {
+        return false;
+    }
 };
+
+
+// If the leapyear is divisible by 4, return true.
+// But if its divisible by 100, its not true.
+// But if its divisible by 100 AND 400, its true. 
 
 // Do not edit below this line
 module.exports = leapYears;
