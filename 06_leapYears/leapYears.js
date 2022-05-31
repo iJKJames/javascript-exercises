b4641202e0beeb46273f7122202dca1c64f33be0
Jachane James <49769620+iJKJames@users.noreply.github.com>
2022-05-31 19:49:54 -0400

    6 of 6 passed. Simplified the order of the conditions

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,16 @@
 const leapYears = function(year) {
-    year = '';
-
-    if (year % 4 == 0 && year % 400 == 0) {
+    if (year % 4 == 0 && year % 100 != 0) {
+        return true;
+    } else {
+        if(year % 400 == 0) {
+            return true;
+        }
         return false;
     }
 };
 
 
-// If the leapyear is divisible by 4, return true.
+// If the year is divisible by 4, return true.
 // But if its divisible by 100, its not true.
 // But if its divisible by 100 AND 400, its true. 
 
