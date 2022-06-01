11b6553666e0561077bb025d66b01516744c971f
Jachane James <49769620+iJKJames@users.noreply.github.com>
2022-05-31 20:15:39 -0400

    6 of 6 passed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(degreesf) {
+  toCelsius = '';
+  toCelsius = ((degreesf - 32) * .555555);
+  return Math.round(toCelsius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(degreesc) {
+  toFahrenheit = '';
+  toFahrenheit = (degreesc * 1.8 + 32);
+  return Math.round(toFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
