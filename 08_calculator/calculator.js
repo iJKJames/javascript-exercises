379ee048af89f0ebf1950c291b891a99cf93cb79
Jachane James <49769620+iJKJames@users.noreply.github.com>
2022-06-01 22:32:35 -0400

    16 of 16 passed.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,34 +1,45 @@
-const add = function(...args) {
+const add = function (...args) {
   return args.reduce((previous, current) => {
     return previous + current;
   });
 };
 
-const subtract = function(...args) {
-	return args.reduce((previous, current) => {
+const subtract = function (...args) {
+  return args.reduce((previous, current) => {
     return previous - current;
   });
 };
 
-const sum = function(array) {
+const sum = function (array) {
   let initialValue = 0;
   let sumWithDefault = array.reduce((previousValue, currentValue) =>
     previousValue + currentValue, initialValue)
   return sumWithDefault;
 };
 
-const multiply = function(array) {
+const multiply = function (array) {
   return array.reduce((previous, current) => {
     return previous * current;
-});
+  });
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+  arr = Array(pow).fill(num);
+  return arr.reduce((previousValue, currentValue) =>
+    previousValue * currentValue)
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  result = num;
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    while (num > 1) {
+      num--;
+      result = result * num;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
